ab5f1c920319abe70d21edb04f491889fa4f72e2
Josh Kallus <joshk.kallus3@gmail.com>
2022-02-07 23:54:34 -0500

    Cleaned up stage tester main

--- MeasurementPlatform/StageControlTester/Program.cs
+++ MeasurementPlatform/StageControlTester/Program.cs
@@ -5,56 +5,15 @@ using StageControl;
 namespace StageControlTester
 {
     internal class Program
-    {
-        //    static SerialPort serialPort;
-
-        //    static void Main(string[] args)
-        //    {
-        //        Console.WriteLine("Hello World!");
-
-        //        Thread readThread = new Thread(Read);
-
-        //        serialPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
-
-        //        serialPort.Open();
-
-        //        readThread.Start();
-
-
-        //        while (true)
-        //        {
-
-        //        }
-
-        //    }
-
-        //    public static void Read()
-        //    {
-        //        while (true)
-        //        {
-        //            try
-        //            {
-        //                string message = serialPort.ReadLine();
-        //                Console.WriteLine(message);
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                Console.WriteLine(e.ToString());
-        //            }
-        //        }
-        //    }
-        //}
-    
+    {    
         static void DataReceived(object sender, SerialDataItemReceivedEventArgs e)
         {
             Console.WriteLine(e.item.ToString());
         }
 
-
         static void Main(string[] args)
         {
             SerialController serialController = new SerialController();
-
             serialController.SerialDataItemReceived += DataReceived;
 
             while(true)
