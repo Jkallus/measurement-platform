95c3e4873311d117aeff49bc6763661a607d45bc
Josh Kallus <joshk.kallus3@gmail.com>
2022-05-27 00:41:32 -0400

    Simplify namespace for this control

--- MeasurementPlatform/MeasurementApp/Controls/Connection/ComponentState.xaml.cs
+++ MeasurementPlatform/MeasurementApp/Controls/Connection/ComponentState.xaml.cs
@@ -16,7 +16,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace MeasurementApp.Controls.Connection
+namespace MeasurementApp.Controls
 {
     public sealed partial class ComponentState : UserControl
     {
