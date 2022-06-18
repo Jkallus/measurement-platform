3cd6ee88e4086b62bc7aa3c3880dbc780c72df48
Josh Kallus <joshk.kallus3@gmail.com>
2022-06-18 16:07:37 -0400

    Added message class and messages for inter VM communication using MVVM toolkit messaging

---
+++ MeasurementPlatform/MeasurementApp/Controls/Stage/StageMessages.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementApp.Controls
+{
+    public class StageTargetPositionChangedMessage: ValueChangedMessage<(float XCoordinate, float YCoordinate)>
+    {
+        public StageTargetPositionChangedMessage((float XCoordinate, float YCoordinate) value) : base(value)
+        {
+            TargetLocation = value;
+        }
+
+        public (float XCoordinate, float YCoordinate) TargetLocation { get; set; }
+        
+    }
+}
