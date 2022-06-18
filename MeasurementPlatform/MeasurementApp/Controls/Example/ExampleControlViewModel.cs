b5aba3814b2275aa9ac1a8f12b0c936d4e159541
Josh Kallus <joshk.kallus3@gmail.com>
2022-06-18 16:32:30 -0400

    Added example user control for testing

---
+++ MeasurementPlatform/MeasurementApp/Controls/Example/ExampleControlViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using StageControl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementApp.Controls
+{
+    public class ExampleControlViewModel: ObservableRecipient
+    {
+
+        // Private member variables
+        private readonly IServiceProvider _service;
+        private readonly StageConfig _stageConfig;
+        private readonly ILogger<ExampleControlViewModel> _logger;
+
+        // Public properties
+        private string _myText;
+        public string MyText
+        {
+            get {  return _myText; }
+            set {  SetProperty(ref _myText, value); }
+        }
+
+
+        public ExampleControlViewModel(IServiceProvider service, ILogger<ExampleControlViewModel> logger)
+        {
+            _service = service;
+            _stageConfig = _service.GetService(typeof(StageConfig)) as StageConfig;
+            _logger = logger;
+            MyText = _stageConfig.ToString();
+            _logger.LogInformation("ExampleControlViewModel Constructed");
+        }
+
+        ~ExampleControlViewModel()
+        {
+            _logger.LogInformation("ExampleControlViewModel Destroyed");
+        }
+
+    }
+}
